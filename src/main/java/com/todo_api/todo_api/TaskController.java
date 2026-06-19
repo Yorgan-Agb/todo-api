4d6fe46b839fe167cd6d4a6e55174f50e449094d
Yorgan-Agb <agblemagnon.yorgan@gmail.com>
2026-06-19 15:05:54 +0200

    complete crud endpoits

--- src/main/java/com/todo_api/todo_api/TaskController.java
+++ src/main/java/com/todo_api/todo_api/TaskController.java
@@ -1,10 +1,8 @@
 package com.todo_api.todo_api;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,10 +17,42 @@ public class TaskController {
         return this.tasks;
     }
     @PostMapping("/tasks")
+    @ResponseStatus(HttpStatus.CREATED)
     public Task createTasks(@RequestBody Task task) {
         task.setId(nextId++);
         tasks.add(task);
         return task;
 
     }
+    @GetMapping("/tasks/{id}")
+    public Task getTaskById(@PathVariable Long id) {
+        for (Task task : this.tasks) {
+           if(task.getId().equals(id)){
+               return task;
+           }
+        }
+        throw new TaskNotFoundException("Task not found");
+    }
+
+    @PutMapping("/tasks/{id}")
+    public Task updateTask(@PathVariable Long id, @RequestBody Task updatedTask){
+        for (Task task : this.tasks) {
+            if(task.getId().equals(id)){
+            task.setTitle(updatedTask.getTitle());
+            task.setDone(updatedTask.isDone());
+            return task;
+            }
+        }
+        throw  new TaskNotFoundException("Task not found");
+    }
+
+    @DeleteMapping("/tasks/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteTask(@PathVariable Long id){
+        boolean removed = this.tasks.removeIf(task -> task.getId().equals(id));
+        if (!removed){
+            throw new TaskNotFoundException("Task to delete doesn't exist");
+        }
+    }
+
 }
